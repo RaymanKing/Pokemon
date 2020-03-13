98a480a7ec0515c9b07094421de15bcce4ecdf53
unknown <enrikerapela@gmail.com>
2020-03-13 17:17:22 +0100

    Limpieza de metodos

--- Main.java
+++ Main.java
@@ -44,15 +44,15 @@ public class Main {
     public Pokemon crearPokemon() {
         String nombre = POKEMON_NOMBRE[(int) Math.floor(Math.random() * POKEMON_NOMBRE.length)];
         int[] tipos = { 0 };
+        int vida = (int) Math.floor(Math.random() * 300 + 1);
+        int atk = (int) Math.floor(Math.random() * 200 + 1);
+        int atksp = (int) Math.floor(Math.random() * 200 + 1);
+        int df = (int) Math.floor(Math.random() * 200 + 1);
+        int dfsp = (int) Math.floor(Math.random() * 200 + 1);
+        int vel = (int) Math.floor(Math.random() * 100 + 1);
+        int id = (int) Math.floor(Math.random() * 1000 + 1);
 
-        Pokemon poke = new Pokemon(nombre, tipos, (int) Math.floor(Math.random() * 20 + 1), /* Vida */
-                (int) Math.floor(Math.random() * 50 + 1), /* Ataque */
-                (int) Math.floor(Math.random() * 20 + 1), /* Ataque especial */
-                (int) Math.floor(Math.random() * 20 + 1), /* Defensa */
-                (int) Math.floor(Math.random() * 20 + 1), /* Defensa especial */
-                (int) Math.floor(Math.random() * 20 + 1), /* Velocidad */
-                (int) Math.floor(Math.random() * 500 + 1), /* Id */
-                crearMovimientos(4));
+        Pokemon poke = new Pokemon(nombre, tipos, vida, atk, atksp, df, dfsp, vel, id, crearMovimientos(4));
 
         return poke;
     }
@@ -61,10 +61,11 @@ public class Main {
     public ArrayList<Movimiento> crearMovimientos(int n) {
         ArrayList<Movimiento> movs = new ArrayList<Movimiento>();
         String nombre = MOVIMIENTOS_NOMBRE[(int) Math.floor(Math.random() * MOVIMIENTOS_NOMBRE.length)];
+        int pp = (int) Math.floor(Math.random() * 30 + 1);
+        int power = (int) Math.floor(Math.random() * 200 + 1);
 
         for (int i = 0; i < n; i++) {
-            movs.add(new Movimiento(nombre, MOVIMIENTOS_TIPO[i], (int) Math.floor(Math.random() * 20 + 1),
-                    (int) Math.floor(Math.random() * 500 + 1)));
+            movs.add(new Movimiento(nombre, MOVIMIENTOS_TIPO[i], pp, power));
         }
         return movs;
     }
