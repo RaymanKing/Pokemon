38c47922b9b39d559d264c550bf2a1cffda9559d
unknown <enrikerapela@gmail.com>
2020-03-19 21:35:25 +0100

    Implementacion del metodo GetInfo y utilizacion en consola

--- Main.java
+++ Main.java
@@ -15,6 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int masinfo = 0;
+
         boolean juego;
 
         System.out.println("Bienvenidos al mundo Pokemon.");
@@ -22,10 +24,26 @@ public class Main {
         System.out.println("Primero vamos a crear al primer entrenador Pokemon");
         Jugador entrenador1 = crearEntrenadorPokemon();
         System.out.println(entrenador1.toString());
+        System.out.println("Si quieres mas informacion de los Pokemons pulse 1");
+        masinfo = sc.nextInt();
+        if (masinfo == 1) {
+            for (int j = 0; j < 6; j++) {
+                System.out.println(entrenador1.getPokemons().get(j).getInfo());
+            }
+            masinfo = 0;
+        }
         System.out.println("Ahora vamos a crear el segundo entrenador Pokemon");
         Jugador entrenador2 = crearEntrenadorPokemon2();
         System.out.println(entrenador2.toString());
-
+        System.out.println("Si quieres mas informacion de los Pokemons pulse 1");
+        masinfo = sc.nextInt();
+        if (masinfo == 1) {
+            for (int j = 0; j < 6; j++) {
+                System.out.println(entrenador2.getPokemons().get(j).getInfo());
+            }
+        }
+        System.out.println("Ahora vamos a comenzar la batalla sientensen y disfruten");
+        // batallaPokemon(entrenador1, entrenador2);
     }
 
     // METODOS PARA LA CREACION DE LOS ENTRENADORES CON SUS 6 POKEMON
@@ -114,11 +132,13 @@ public class Main {
     // METODO BATALLA POKEMON
 
     /*
-     * public String batallaPokemon(Jugador entrenador1,Jugador entrenador2){ int i
-     * = (int) (Math.random()*6+1); int j = (int) (Math.random()*6+1);
+     * public String batallaPokemon(Jugador entrenador1, Jugador entrenador2) { int
+     * i = (int) (Math.random() * 6 + 1); int j = (int) (Math.random() * 6 + 1);
      * System.out.println("Bienvenido a la batalla Pokemons");
-     * System.out.println("Entre los entrenadores "+entrenador1.getNombre());
-     * System.out.println("contra el entrenador "+entrenador2.getNombre()); String
-     * nombre = entrenador1.getPokemons(i).getNombre(); }*
+     * System.out.println("Entre los entrenadores " + entrenador1.getNombre());
+     * System.out.println("contra el entrenador " + entrenador2.getNombre());
+     * 
+     * }
      */
+
 }
